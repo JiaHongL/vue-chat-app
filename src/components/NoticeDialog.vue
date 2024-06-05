77af77345ce97a67a2b7b04606225633fd9ea4b5
joe <>
2024-06-06 07:11:25 +0800

    增加共用通知視窗

---
+++ src/components/NoticeDialog.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import DialogCloseButton from '@/components/DialogCloseButton.vue';
+import useNotice from '@/composables/useNotice';
+import type { PropType } from 'vue';
+
+const props = defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  data: {
+    type: Object as PropType<{ message: string }>,
+    default: () => ({ message: '' }),
+  },
+});
+
+const { closeDialog } = useNotice();
+
+</script>
+
+<template>
+    <div class="bg-white p-8 rounded-lg shadow-lg w-96 sm:max-w-md sm:w-full">
+      <div class="flex justify-between items-center mb-6">
+        <h2 id="notification-title" class="text-2xl font-bold text-gray-900">Notice</h2>
+        <DialogCloseButton @click="closeDialog(props.id)"/>
+      </div>
+      <p id="notification-message" class="text-gray-700 mb-4">
+        {{ data.message }}
+      </p>
+      <div class="flex justify-center">
+        <button
+          @click="closeDialog(props.id)"
+          class="py-2 px-4 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-200"
+        >close</button>
+      </div>
+    </div>
+</template>
---
+++ src/composables/useNotice.ts
@@ -0,0 +1,24 @@
+import { useDialogStore } from "@/stores/dialogStore";
+import NoticeDialog from "@/components/NoticeDialog.vue";
+
+
+const useNotice = () => {
+  const dialogStore = useDialogStore();
+
+  const notice = (message: string) => {
+    return dialogStore.addDialog(NoticeDialog, { data: { message } });
+  };
+
+  const closeDialog = (id: string, result:any = null) => {
+    const dialog = dialogStore.dialogs.find(dialog => dialog.id === id);
+    dialog._onClose(result);
+  } 
+
+  return {
+    notice,
+    closeDialog
+  };
+
+};
+
+export default useNotice;
