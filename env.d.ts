dc8545e252f612e9454c2d5937b053f3b66f9917
joe <>
2024-06-06 00:59:16 +0800

    增加 API 環境變數

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  /** backend api base url */
+  readonly VITE_API_BASE_URL: string;
+  /** websocket base url */
+  readonly VITE_WS_BASE_URL: string;
+  /** profile image api base url */
+  readonly VITE_PROFILE_IMAGE_API: string;
+}
