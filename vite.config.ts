34db32d30ec64011fc6f6c062ad444fe93de6feb
joe <>
2024-06-08 23:22:12 +0800

    set deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import Components from "unplugin-vue-components/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue-chat-app/',
   plugins: [
     vue(),
     Components({
